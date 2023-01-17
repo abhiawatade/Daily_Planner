c76488dc1e96c77f21de673012b7802142d0c484
Abhishek Awatade <86554091+abhiawatade@users.noreply.github.com>
2023-01-18 00:16:56 +0530

    Revert "feat :web push notifications (#1)"
    This reverts commit 7cf6c5e9fde31ab2d67c0d584e288adcd7df03cc.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,33 +1,14 @@
 const RedisClient = require("../config/connectRedis");
-const webpush = require("web-push");
-//vapid keys
-const publicVapidKey =
-  "BJg8uwvEr5FwjPSxAuWxjU126WL3HDjQPq2a9z3d_jpjrXPHO0vS2opSKuXDnUJB1QiBPfG0kv7CeVtm15o2Ruo";
-
-const privateVapidKey = "H_oHbxzRsnkZQ7DzglX5jgI6MkPnW2PpgvdFmW5gl4E";
-
-webpush.setVapidDetails(
-  "mailto:test@test.com",
-  publicVapidKey,
-  privateVapidKey
-);
 
 const createSchedule = async (req, res) => {
   try {
     // "schedule":["workout","","breakfast"]
-    const { schedule, pushSubscription } = req.body;
+    const { schedule } = req.body;
     console.log(schedule);
 
     await RedisClient.set("schedule", JSON.stringify(schedule));
-    await RedisClient.set("pushSubscription", JSON.stringify(pushSubscription));
-    const savedSchedule = await RedisClient.get("schedule");
 
-    webpush
-      .sendNotification(
-        pushSubscription,
-        JSON.stringify({ title: "paradox", body: "be paradox" })
-      )
-      .catch(console.log);
+    const savedSchedule = await RedisClient.get("schedule");
 
     res.status(200).json({ message: savedSchedule });
   } catch (error) {
@@ -37,3 +18,6 @@ const createSchedule = async (req, res) => {
 };
 
 module.exports = createSchedule;
+
+//createSchedule need to be function You can't pass it as object "{createSchedule}"
+//it's callback required scheduleRoutes
